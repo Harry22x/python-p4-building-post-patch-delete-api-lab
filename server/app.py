416b7251dc76ba7e4e41fd82b2eba74a85dcedb9
Sitoh7 <ha.wakhule@gmail.com>
2025-01-19 15:23:16 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -47,3 +47,40 @@ def most_expensive_baked_good():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/baked_goods', methods = ['POST'])
+def post_baked_goods():
+    new_good = BakedGood(name = request.form.get("name"),
+                         price = request.form.get("price"),
+                         bakery_id = request.form.get("bakery_id"))
+    db.session.add(new_good)
+    db.session.commit()
+
+    new_good_dict = new_good.to_dict()
+    response = make_response(new_good_dict,201)
+    return response
+
+@app.route('/bakeries/<int:id>',methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.filter_by(id=id).first()
+    for attr in request.form:
+        setattr(bakery,attr,request.form.get(attr))
+    
+    db.session.add(bakery)
+    db.session.commit()
+
+    new_bakery_dict = bakery.to_dict()
+    response = make_response(new_bakery_dict,200)
+    return response
+    
+@app.route('/baked_goods/<int:id>',methods=['DELETE'])
+def delete_good(id):
+    good = BakedGood.query.filter_by(id=id).first()
+    db.session.delete(good)
+    db.session.commit()
+    response_body ={
+        "delete_successful":True,
+        "message":"good deleted"
+    }
+    response = make_response(response_body,200)
+    return response
